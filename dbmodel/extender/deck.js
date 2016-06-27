5ef396dc6f28c420c03cc75f378535c3a4066ef2
qwemaze <qwelias@gmail.com>
2016-06-27 21:48:39 +0500

    add css autoprefixer, init decktape server api

--- dbmodel/extender/deck.js
+++ dbmodel/extender/deck.js
@@ -1,10 +1,13 @@
 "use strict";
 
 const deckBuilder = require( '../../lib/reveal' );
+const deckTape = require( '../../lib/deckTape' );
 const Log = require( 'debug' )( 'app:model:extender:deck' );
 
 const useSchema = ( schema ) => {
 	schema.post( 'save', ( doc ) => deckBuilder( doc.toObject() ).catch( ( e ) => Log( e.stack || e ) ) );
+
+	schema.method( 'toPDF', deckTape );
 };
 
 const useModel = ( model ) => {
